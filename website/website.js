ab3b459a1ff32a20422f6fd023a706348e5fdcc7
Navya S Narayan <navyasn99@gmail.com>
2015-01-19 17:17:51 +0530

    Added buttons and CSS styling

--- website/website.js
+++ website/website.js
@@ -3,3 +3,12 @@ function display (selected)
 	document.getElementById("text").innerHTML = "Text related to " + selected + " project"
 }
 
+function user (name)
+{
+	document.getElementById("text").innerHTML = "Text related to " + name 
+}
+
+function Info(){
+	document.getElementById("texty").style.display="none";
+	document.getElementById("home").style.display="block";
+}
