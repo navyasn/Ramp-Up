b4c09ec4bc5a99d68b9d161e8d026945f4cb9324
Navya S Narayan <navyasn99@gmail.com>
2015-01-27 17:34:04 +0530

    Added a line break for better visibility

--- website/website.js
+++ website/website.js
@@ -1,14 +1,19 @@
 function display (selected)
 {
 	document.getElementById("text").innerHTML = "Text related to " + selected + " project";
+	document.getElementById("home").style.display="block";
+	document.getElementById("texty").style.display="none";
 }
 
 function user (name)
 {
 	document.getElementById("text").innerHTML = "Text related to " + name;
+	document.getElementById("home").style.display="block";
+	document.getElementBystyle.verticalAlignId("texty").style.display="none";
 }
 
-function Info(){
+function Info()
+{
 	document.getElementById("texty").style.display="none";
 	document.getElementById("home").style.display="block";
 }
