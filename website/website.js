9dc0c74f72c6ee954e57784fde4bed7eb2ed6906
Navya S Narayan <navyasn99@gmail.com>
2015-02-02 17:34:04 +0530

    Changed the div name

--- website/website.js
+++ website/website.js
@@ -1,15 +1,16 @@
 function display (selected)
 {
-	document.getElementById("text").innerHTML = "Text related to " + selected + " project";
+	document.getElementById("information").innerHTML = "Text related to " + selected + " project";
 	document.getElementById("home").style.display="block";
 	document.getElementById("texty").style.display="none";
+    
 }
 
 function user (name)
 {
-	document.getElementById("text").innerHTML = "Text related to " + name;
+	document.getElementById("information").innerHTML = "Text related to " + name;
 	document.getElementById("home").style.display="block";
-	document.getElementBystyle.verticalAlignId("texty").style.display="none";
+	document.getElementBysId("texty").style.display="none";
 }
 
 function Info()
